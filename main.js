5e4da91bac1e6f5519d4f839fe6e29cc49f872a0
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-05-17 13:41:32 +0200

    over

---
+++ main.js
@@ -0,0 +1,38 @@
+let softSkills = {
+    Trello: '49%',
+    Git: '87%',
+    GitHub : '50%',
+    Discord: '15%',
+}
+
+let elements = document.querySelectorAll("#liste-soft-skills > h2")
+let i = 0;
+console.log(elements);
+// console.log(softSkills);
+// console.log(softSkills.Trello);
+for(let el in softSkills){
+    elements.forEach(skill => {
+        if (skill.innerText == el) {
+            elements[i].innerText += " "
+        }
+    });
+
+    
+
+    let titre = elements[i]
+    titre.innerText = `${softSkills[el]}`
+
+    let competLength = softSkills[el].length
+    let verif = softSkills[el].substr(0, competLength - 1)
+    // pour avoir juste le personnage !
+    if (verif > 50 && verif < 100 && verif !=50){ // priorité logique !!
+        titre.style.backgroundColor = "green"
+        titre.style.color = "white"
+    } else if (verif == 100){
+        titre.style.backgroundColor = "gold"
+    }else if (verif < 50){
+        titre.style.backgroundColor ="red"
+    }
+    titre.style.width = `${verif}%`
+    i++
+}
